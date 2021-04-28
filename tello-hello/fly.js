9dd7497b2838737662c07e2958a53b72f1c69569
jeff14994 <jeff14994@gmail.com>
2021-04-29 01:14:25 +0800

    Fix the offset of Tello's movement

--- tello-hello/fly.js
+++ tello-hello/fly.js
@@ -5,7 +5,7 @@ const app = require('express')();
 const http = require('http').Server(app);
 const throttle = require('lodash/throttle');
 const io = require('./node_modules/socket.io')(http);
-const commandDelay = require('../backend/commandDelays');
+const commandDelays = require('../backend/commandDelays');
 
 const PORT = 8889;
 const HOST = '192.168.10.1';
@@ -54,13 +54,14 @@ function handleError(err){
 };
 // Send data to drone 
 // Initialize drone by sending 'command' to the tello server
+console.log('Send command: Command');
 drone.send('command', 0, 'command'.length, PORT, HOST, handleError);
 // drone.send('battery?', 0, 'battery?'.length, PORT, HOST, handleError);
 // drone.send('time?', 0, 'time?'.length, PORT, HOST, handleError);
 // drone.send('wifi?', 0, 'wifi?'.length, PORT, HOST, handleError);
 // const commands = ['command', 'battery?', 'takeoff', 'land'];
-const commands = ['command', 'battery?'];
-let i = 0;
+// const commands = ['command', 'battery?'];
+// let i = 0;
 // async function go(){
 //     const command = commands[i];
 //     const delay = commandDelay[command];
@@ -79,16 +80,16 @@ let i = 0;
 
 
 
-// //   [
-// //     [ 'pitch', '0' ],  [ 'roll', '0' ],
-// //     [ 'yaw', '-85' ],  [ 'vgx', '0' ],
-// //     [ 'vgy', '0' ],    [ 'vgz', '0' ],
-// //     [ 'templ', '92' ], [ 'temph', '94' ],
-// //     [ 'tof', '10' ],   [ 'h', '0' ],
-// //     [ 'bat', '86' ],   [ 'baro', '132.84' ],
-// //     [ 'time', '0' ],   [ 'agx', '2.00' ],
-// //     [ 'agy', '6.00' ], [ 'agz', '-999.00' ],
-// //     [ '\r\n' ]
+//   [
+//     [ 'pitch', '0' ],  [ 'roll', '0' ],
+//     [ 'yaw', '-85' ],  [ 'vgx', '0' ],
+//     [ 'vgy', '0' ],    [ 'vgz', '0' ],
+//     [ 'templ', '92' ], [ 'temph', '94' ],
+//     [ 'tof', '10' ],   [ 'h', '0' ],
+//     [ 'bat', '86' ],   [ 'baro', '132.84' ],
+//     [ 'time', '0' ],   [ 'agx', '2.00' ],
+//     [ 'agy', '6.00' ], [ 'agz', '-999.00' ],
+//     [ '\r\n' ]
 
 // Set ejs as frontend render engine
 app.set('view engine', 'ejs');
@@ -100,24 +101,29 @@ app.get('/', (req, res) => {
 
 http.listen(8000, () => {
     console.log('Socket io server up and running');
+    console.log('Please wait Tello to stabilize');
 });
 // Socket Server
 io.sockets.on('connection', socket => {
     socket.on('command', command => {
-      console.log('command Sent from browser');
-      console.log(command);
-      drone.send(command, 0, command.length, PORT, HOST, handleError);
+        // command_filter = command.split(' ')[0];
+        // console.log(command_filter);
+        // const delay = commandDelays[command_filter];
+        // console.log(delay);
+        console.log('command Sent from browser');
+        console.log(command);
+        drone.send(command, 0, command.length, PORT, HOST, handleError);
+        // sleep(delay);
     });
-  
-    socket.emit('status', 'CONNECTED');
-  });
+    // socket.emit('status', 'CONNECTED');
+});
 
-// Receive fomatted drone data
+// Receive formatted drone data
 droneState.on(
     'message',
     throttle(state => {
     const formattedState = parseState(state.toString());
-    console.log(formattedState);
+    // console.log(formattedState);
     io.sockets.emit('dronestate', formattedState);
     }, 100)
 );
